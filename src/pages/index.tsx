1a2d9ac9404a2d3e592ee039b193daa8253883dc
Tuan Luu <tuan.luu@asnet.com.vn>
2022-04-02 13:47:28 +0700

    Init project

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1 className="text-red-500">Hello World</h1>
+    </div>
+  )
+}
+
+export default Home
